2fc4cdd6d3785aea905f418bbbdbe3eb01f36205
Asada Shino <shinon520@gmail.com>
2022-01-06 16:35:40 +0800

    complete ban, kick, enable and disable functions

--- include/module.hpp
+++ include/module.hpp
@@ -4,7 +4,7 @@
 using namespace std;
 using namespace Cyan;
 
-const string func_names[] = {"anti-recall", "repeat-count", "ban", "kick"};
+const set<string> func_names = {"anti-recall", "repeat-count", "ban", "kick"};
 class Module {
 private:
     map<GID_t, map<string, bool>> group_settings;
@@ -12,9 +12,16 @@ private:
     set<QQ_t> black_list;
     set<GID_t> enabled_group_list;
     json config;
+    MiraiBot bot;
+    SessionOptions opts;
 private:
     void load_config_file();
     void save_config_file();
+    bool enable(Group_t& group, GroupMember& sender, string func_name);
+    bool disable(Group_t& group, GroupMember& sender, string func_name);
+    void kick(Group_t& group, GroupMember& sender, QQ_t target, string reason);
+    void ban(Group_t& group, GroupMember& sender, QQ_t target, int time, string reason);
+    void command_parser(vector<string>& cmd, string plain_text);
 public:
     Module();
     void init();
--- src/module.cpp
+++ src/module.cpp
@@ -1,9 +1,52 @@
 #include "module.hpp"
+#include "information.hpp"
 #include <fstream>
 
-Module::Module() {}
+Module::Module() {
+    opts.BotQQ = BOT_QQ;				
+	opts.HttpHostname = "localhost";		
+	opts.WebSocketHostname = "localhost";	
+	opts.HttpPort = 8080;					
+	opts.WebSocketPort = 8080;				
+	opts.VerifyKey = VERIFY_CODE;
+}
 
 void Module::init() {
+    while(true)
+	{
+		try
+		{
+			bot.Connect(opts);
+            cout << "bot working" << endl;
+			break;
+		}
+		catch (const std::exception& ex)
+		{
+			cout << ex.what() << endl;
+		}
+		MiraiBot::SleepSeconds(1);
+	}
+	bot.On<GroupMessage>([&](GroupMessage m){deal_group_message(m);});
+	bot.On<LostConnection>([&](LostConnection e)
+		{
+			cout << e.ErrorMessage << " (" << e.Code << ")" << endl;
+			while (true)
+			{
+				try
+				{
+					cout << "尝试连接 mirai-api-http..." << endl;
+					bot.Reconnect();
+					cout << "与 mirai-api-http 重新建立连接!" << endl;
+					break;
+				}
+				catch (const std::exception& ex)
+				{
+					cout << ex.what() << endl;
+				}
+				MiraiBot::SleepSeconds(5);
+			}
+		});
+
     load_config_file();
 }
 
@@ -48,9 +91,89 @@ void Module::save_config_file() {
 }
 
 void Module::release() {
+	bot.Disconnect();
     save_config_file();
 }
 
 void Module::deal_group_message(GroupMessage m) {
-    
+    try {
+        if(enabled_group_list.count(m.Sender.Group.GID) > 0) {
+            vector<string> cmd;
+            command_parser(cmd, m.MessageChain.GetPlainText());
+            for(auto s : cmd) {
+                cout << s << endl;
+            }
+            if(cmd.size() == 2 && cmd[0] == "enable") {
+                if(enable(m.Sender.Group, m.Sender, cmd[1]))
+                    m.QuoteReply(MessageChain().Plain(cmd[1]+"功能已启用。"));
+            }
+            else if(cmd.size() == 2 && cmd[0] == "disable") {
+                if(disable(m.Sender.Group, m.Sender, cmd[1]))
+                    m.QuoteReply(MessageChain().Plain(cmd[1]+"功能已禁用。"));
+            }
+            else if(cmd.size() >= 2 && cmd[0] == "ban" && group_settings[m.Sender.Group.GID]["ban"] == true) {
+                ban(m.Sender.Group, m.Sender, m.MessageChain.GetFirst<AtMessage>().Target(), stol(cmd[1]), cmd.size() >=3?cmd[2]:"");
+            }
+            else if(cmd.size() >= 1 && cmd[0] == "kick" && group_settings[m.Sender.Group.GID]["kick"] == true) {
+                kick(m.Sender.Group, m.Sender, m.MessageChain.GetFirst<AtMessage>().Target(), cmd.size() >= 2? cmd[1] : "");
+            }
+        }
+    }
+    catch (const std::exception& ex) {
+			cout << ex.what() << endl;
+		}
+}
+
+bool Module::enable(Group_t& group, GroupMember& sender, string func_name) {
+    if(super_admin_list.count(sender.QQ) > 0 && func_names.count(func_name) > 0 && group_settings[group.GID][func_name] == false) {
+        group_settings[group.GID][func_name] = true;
+        config["group_settings"][to_string(group.GID.ToInt64())][func_name] = true;
+        return true;
+    }
+    return false;
+}
+
+bool Module::disable(Group_t& group, GroupMember& sender, string func_name) {
+    if(super_admin_list.count(sender.QQ) > 0 && func_names.count(func_name) > 0 && group_settings[group.GID][func_name] == true) {
+        group_settings[group.GID][func_name] = false;
+        config["group_settings"][to_string(group.GID.ToInt64())][func_name] = false;
+        return true;
+    }
+    return false;
+}
+
+void Module::command_parser(vector<string>& cmd, string plain_text) {
+    int start = 0;
+    bool blank = false;
+    for(int i = 0 ; i < plain_text.length(); ++i) {
+        if(plain_text[i] == ' ' || plain_text[i] == '\n') {
+            if(blank == false) {
+                cmd.push_back(plain_text.substr(start, i-start));
+                blank = true;
+            }
+        }
+        else {
+            if(blank == true) {
+                start = i;
+                blank = false;
+            }
+        }
+    }
+    if(blank == false)
+        cmd.push_back(plain_text.substr(start));
+}
+
+void Module::ban(Group_t& group, GroupMember& sender, QQ_t target, int minute, string reason) {
+    if((sender.Permission >= GroupPermission::Administrator || super_admin_list.count(sender.QQ) > 0) && super_admin_list.count(target) == 0) {
+        if(minute == 0)
+            bot.UnMute(group.GID, target);
+        if(minute > 0 && minute <= 43200)
+        bot.Mute(group.GID, target, minute*60);
+    }
+}
+
+void Module::kick(Group_t& group, GroupMember& sender, QQ_t target, string reason) {
+    if((sender.Permission >= GroupPermission::Administrator || super_admin_list.count(sender.QQ) > 0) && super_admin_list.count(target) == 0) {
+        bot.Kick(group.GID, target);
+    }
 }
